d16615b0f3238b52e46f16cc6dab4cbed51fbe8b
kongdong <1216181067@qq.com>
2021-08-23 10:34:45 +0800

    设计模式

--- react-inspector/src/main.tsx
+++ react-inspector/src/main.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+ import App from './App'
+
+// function App() {
+//   const [text, setText] = React.useState('hello word');
+//   const [name, setNam] = React.useState('Jack');
+//   return <div>{text}</div>
+// }
 const element = <App />;
 ReactDOM.render(element,document.getElementById('root'))
